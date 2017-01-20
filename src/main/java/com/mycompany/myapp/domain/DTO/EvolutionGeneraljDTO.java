e7774c1153121aaf0394e372c1da0c76ce0507da
Alan <alanvallve@gmail.com>
2017-01-20 17:04:55 +0100

    Evolucion Player

---
+++ src/main/java/com/mycompany/myapp/domain/DTO/EvolutionGeneraljDTO.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp.domain.DTO;
+
+import com.mycompany.myapp.domain.Player;
+
+/**
+ * Created by Alan on 20/01/2017.
+ */
+public class EvolutionGeneraljDTO {
+
+    private Player player;
+    private JugadorDTO playerDTO;
+}
